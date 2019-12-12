988e69d56f2c1f718da212bbc4c9bd1425d2d0b3
Tim <timrogers32@gmail.com>
2019-12-11 16:48:17 -0800

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    //Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int digit, sum = 0, prod = 1, temp = Math.Abs(n);
+            if (n <= 0)
+            {
+                return 0;
+            }
+            while (temp != 0)
+            {
+                digit = temp % 10;
+                sum += digit;
+                prod *= digit;
+                temp /= 10;
+            }
+            return prod - sum;
+        }
+    }
+}
